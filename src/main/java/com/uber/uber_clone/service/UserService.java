df5a455cdf5e663a580ff892d313664661715fc2
Dheeraj Pawar <dheerajpawar242@gmail.com>
2026-03-02 08:18:18 +0530

    Day 13: Auto-create driver profile on DRIVER registration

--- src/main/java/com/uber/uber_clone/service/UserService.java
+++ src/main/java/com/uber/uber_clone/service/UserService.java
@@ -1,6 +1,8 @@
 package com.uber.uber_clone.service;
 
+import com.uber.uber_clone.entity.Driver;
 import com.uber.uber_clone.entity.User;
+import com.uber.uber_clone.repository.DriverRepository;
 import com.uber.uber_clone.repository.UserRepository;
 import com.uber.uber_clone.utility.DistanceUtil;
 
@@ -19,10 +21,25 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final DriverRepository driverRepository;
 
     public User register(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+        // ===== Day 13 Logic =====
+        if (savedUser.getRole().name().equals("DRIVER")) {
+
+            Driver driver = new Driver();
+            driver.setUser(savedUser);
+            driver.setAvailable(false);
+            driver.setCurrentLatitude(0.0);
+            driver.setCurrentLongitude(0.0);
+            driver.setVehicleNumber("TEMP-" + savedUser.getId());
+
+            driverRepository.save(driver);
+        }
+        // ========================
+        return savedUser;
     }
     //day 3
     public User login(LoginRequest request) {
